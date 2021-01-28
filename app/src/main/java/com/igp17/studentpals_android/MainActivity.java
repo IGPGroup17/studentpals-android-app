976d7f459c6aa7acf5f7e64a2663db0e554111fa
Ollie <olliejonas@gmail.com>
2021-01-28 18:02:20 +0000

    Initial commit

---
+++ app/src/main/java/com/igp17/studentpals_android/MainActivity.java
@@ -0,0 +1,15 @@
+package com.igp17.studentpals_android;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        System.out.println("hi");
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}

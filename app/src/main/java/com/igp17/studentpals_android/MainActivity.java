013c16f99dbb9128a0b39819d175938a9b11f73e
Ollie <olliejonas@gmail.com>
2021-01-28 18:02:38 +0000

    Update MainActivity.java

--- app/src/main/java/com/igp17/studentpals_android/MainActivity.java
+++ app/src/main/java/com/igp17/studentpals_android/MainActivity.java
@@ -8,7 +8,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        System.out.println("hi");
+        System.out.println("hi there");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
